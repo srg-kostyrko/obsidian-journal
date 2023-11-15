0640694c176d638c33a7d70617ead8e0c87ea9b8
Sergii Kostyrko <srg.kostyrko@gmail.com>
2023-11-15 14:37:48 +0200

    feat: open today's node, creating new if missing

--- main.ts
+++ main.ts
@@ -1,9 +1,17 @@
-import { Notice, Plugin } from "obsidian";
+import { Plugin, TFile, moment } from "obsidian";
+
+const NAME_FORMAT = "YYYY-MM-DD";
 
 export default class JournalPlugin extends Plugin {
   async onload() {
-    this.addRibbonIcon("dice", "Greet", () => {
-      new Notice("Hello, world!");
+    this.addRibbonIcon("calendar-days", "Open daily note", async () => {
+      const filename = moment().format(NAME_FORMAT) + ".md";
+      let file = this.app.vault.getAbstractFileByPath(filename);
+      if (!file) {
+        file = await this.app.vault.create(filename, "");
+      }
+      const leaf = this.app.workspace.getLeaf();
+      await leaf.openFile(file as TFile, { active: true });
     });
   }
 }
