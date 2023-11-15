cfe61965ea1045f07a1876cde9d1d1b6852d6ffa
Sergii Kostyrko <srg.kostyrko@gmail.com>
2023-11-15 09:53:45 +0200

    chore: boostrap plugin

---
+++ main.ts
@@ -0,0 +1,9 @@
+import { Notice, Plugin } from "obsidian";
+
+export default class JournalPlugin extends Plugin {
+  async onload() {
+    this.addRibbonIcon('dice', 'Greet', () => {
+      new Notice('Hello, world!');
+    });
+  }
+}
