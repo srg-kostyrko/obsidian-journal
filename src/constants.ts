2de8845758496cf9b2f9f2c51f00c9319b1086d5
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-10-24 22:20:53 +0300

    feat: apply selected shelf in calendar view

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,6 @@
+import { type InjectionKey } from "vue";
+import type { ProvidedShelfData } from "./types/provided-data.types";
+
 export const CALENDAR_VIEW_TYPE = "journal-calendar";
 
 export const FRONTMATTER_DATE_FORMAT = "YYYY-MM-DD";
@@ -5,3 +8,5 @@ export const FRONTMATTER_NAME_KEY = "journal";
 export const FRONTMATTER_DATE_KEY = "journal-date";
 export const FRONTMATTER_END_DATE_KEY = "journal-end-date";
 export const FRONTMATTER_INDEX_KEY = "journal-index";
+
+export const SHELF_DATA_KEY = Symbol() as InjectionKey<ProvidedShelfData>;
