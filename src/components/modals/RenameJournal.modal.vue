7167a7d460046caae382f68b5ac532c59a02e731
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-12-10 17:28:16 +0200

    refactor: update rename journal modal

--- src/components/modals/RenameJournal.modal.vue
+++ src/components/modals/RenameJournal.modal.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
 import ObsidianSetting from "../obsidian/ObsidianSetting.vue";
 import ObsidianTextInput from "../obsidian/ObsidianTextInput.vue";
 import ObsidianButton from "../obsidian/ObsidianButton.vue";
 import { usePlugin } from "@/composables/use-plugin";
+import { useForm } from "vee-validate";
+import { toTypedSchema } from "@vee-validate/valibot";
+import * as v from "valibot";
+import FormErrors from "../FormErrors.vue";
 
 const props = defineProps<{
   name: string;
@@ -15,33 +18,48 @@ const emit = defineEmits<{
 
 const plugin = usePlugin();
 
-const newName = ref(props.name);
-
-const canSave = computed(() => {
-  return newName.value.length > 0 && newName.value !== props.name && !plugin.hasJournal(newName.value);
-});
-const showUniqeWarning = computed(() => {
-  return newName.value && newName.value !== props.name && !plugin.hasJournal(newName.value);
-});
-
-function save() {
-  emit("save", newName.value);
-  emit("close");
+function isNameNotUnique(value: string) {
+  return value === props.name || (value !== props.name && !plugin.hasJournal(value));
 }
+
+const { defineField, errorBag, handleSubmit } = useForm({
+  initialValues: {
+    journalName: props.name,
+  },
+  validationSchema: toTypedSchema(
+    v.object({
+      journalName: v.pipe(
+        v.string(),
+        v.nonEmpty("Journal name is required"),
+        v.check(isNameNotUnique, "Journal name should be unique"),
+      ),
+    }),
+  ),
+});
+const [journalName, journalNameAttrs] = defineField("journalName");
+
+const onSubmit = handleSubmit((values) => {
+  if (values.journalName !== props.name) {
+    emit("save", values.journalName);
+  }
+  emit("close");
+});
 </script>
 
 <template>
-  <ObsidianSetting name="New name">
-    <template #description>
-      Renaming journal will require restart if you have ribbon icons and/or commands configured.
-      <span v-if="showUniqeWarning" class="journal-important">This name is already used.</span>
-    </template>
-    <ObsidianTextInput v-model="newName" />
-  </ObsidianSetting>
-  <ObsidianSetting>
-    <ObsidianButton @click="$emit('close')">Cancel</ObsidianButton>
-    <ObsidianButton cta :disabled="!canSave" @click="save">Save</ObsidianButton>
-  </ObsidianSetting>
+  <form @submit.prevent="onSubmit">
+    <ObsidianSetting name="New name">
+      <template #description>
+        Renaming journal will require restart if you have ribbon icons and/or commands configured.
+        <FormErrors :errors="errorBag.journalName" />
+      </template>
+      <ObsidianTextInput v-model="journalName" v-bind="journalNameAttrs" />
+    </ObsidianSetting>
+    <ObsidianSetting>
+      <ObsidianButton @click="$emit('close')">Cancel</ObsidianButton>
+      <ObsidianButton cta type="submit">Save</ObsidianButton>
+    </ObsidianSetting>
+  </form>
 </template>
 
 <style scoped></style>
