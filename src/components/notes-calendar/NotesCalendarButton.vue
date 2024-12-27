e65f40d85abcce52ec4f178e889356876a105bdb
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-12-27 15:18:43 +0200

    feat: add context menu to note buttons

--- src/components/notes-calendar/NotesCalendarButton.vue
+++ src/components/notes-calendar/NotesCalendarButton.vue
@@ -9,6 +9,8 @@ import { openDate } from "@/journals/open-date";
 import { usePlugin } from "@/composables/use-plugin";
 import type { JournalSettings } from "@/types/settings.types";
 import { calendarFormats } from "@/constants";
+import { Menu } from "obsidian";
+import type { JournalNoteData } from "@/types/journal.types";
 
 const { date, type } = defineProps<{
   date: string;
@@ -30,10 +32,52 @@ function open(event: MouseEvent) {
     event,
   ).catch(console.error);
 }
+
+function openContextMenu(event: MouseEvent) {
+  const notes: JournalNoteData[] = [];
+  for (const journal of journals[type].value) {
+    const data = journal.get(date);
+    if (data && "path" in data) {
+      notes.push(data);
+    }
+  }
+  if (notes.length === 0) return;
+  if (notes.length === 1) {
+    showContextMenuForPath(notes[0].path, event);
+  } else {
+    const menu = new Menu();
+    for (const note of notes) {
+      menu.addItem((item) => {
+        item.setTitle(note.path).onClick(() => {
+          showContextMenuForPath(note.path, event);
+        });
+      });
+    }
+    menu.showAtMouseEvent(event);
+  }
+}
+
+function showContextMenuForPath(path: string, event: MouseEvent): void {
+  const file = plugin.app.vault.getAbstractFileByPath(path);
+  if (file) {
+    const menu = new Menu();
+    plugin.app.workspace.trigger("file-menu", menu, file, "file-explorer-context-menu", null);
+    menu.addItem((item) =>
+      item
+        .setTitle("Delete")
+        .setIcon("trash")
+        .onClick(() => {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-explicit-any
+          (plugin.app.fileManager as any).promptForFileDeletion(file);
+        }),
+    );
+    menu.showAtMouseEvent(event);
+  }
+}
 </script>
 
 <template>
-  <CalendarButton class="calendar-button" :clickable="isActionable" @click="open">
+  <CalendarButton class="calendar-button" :clickable="isActionable" @click="open" @contextmenu="openContextMenu">
     <CalendarDecoration :styles="decorationsStyles">
       <FormattedDate :date :format />
     </CalendarDecoration>
