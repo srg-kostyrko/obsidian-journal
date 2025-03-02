ba9dffe86eb54d2e74b9db22549c278b8ad48e91
Sergey Kostyrko <srg.kostyrko@gmail.com>
2025-03-02 17:02:56 +0200

    chore: remove old code

--- src/_old-code/utils.ts
+++
@@ -1,6 +0,0 @@
-export function deepCopy<T>(input: T): T {
-  return JSON.parse(JSON.stringify(input));
-}
-export function capitalize(str: string): string {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
