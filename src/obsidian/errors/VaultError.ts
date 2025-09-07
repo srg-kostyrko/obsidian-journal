f51875838ae13712ad018736ce390a59a5a49d52
Sergey Kostyrko <srg.kostyrko@gmail.com>
2025-09-07 17:50:46 +0300

    fix: avoid recreating VaultError

--- src/obsidian/errors/VaultError.ts
+++ src/obsidian/errors/VaultError.ts
@@ -1,5 +1,6 @@
 export class VaultError extends Error {
   public static fromCatch(error: unknown) {
+    if (error instanceof VaultError) return error;
     return new VaultError(error instanceof Error ? error.message : String(error));
   }
 }
