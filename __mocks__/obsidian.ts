d210f5351024ef2f6926c74625de7d196a8b4b79
Sergii Kostyrko <srg.kostyrko@gmail.com>
2026-05-13 21:01:35 +0300

    build(host): add nanoevents and extend obsidian mock with TFile/TFolder

--- __mocks__/obsidian.ts
+++ __mocks__/obsidian.ts
@@ -1,2 +1,30 @@
 import moment from "moment";
+
 export { moment };
+
+export class TAbstractFile {
+  path = "";
+  name = "";
+  parent: TFolder | null = null;
+}
+
+export class TFile extends TAbstractFile {
+  basename = "";
+  extension = "";
+  stat = { ctime: 0, mtime: 0, size: 0 };
+}
+
+export class TFolder extends TAbstractFile {
+  children: TAbstractFile[] = [];
+
+  isRoot(): boolean {
+    return this.parent === null;
+  }
+}
+
+export function normalizePath(path: string): string {
+  return path
+    .replaceAll(/\\/g, "/")
+    .replace(/\/{2,}/g, "/")
+    .replace(/^\/|\/$/g, "");
+}
