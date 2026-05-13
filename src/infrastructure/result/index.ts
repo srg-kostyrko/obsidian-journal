be893e1dc97596baecea2b477da922c6c72e95ce
Sergii Kostyrko <srg.kostyrko@gmail.com>
2026-05-13 15:30:02 +0300

    feat(result): add public barrel for src/infrastructure/result

---
+++ src/infrastructure/result/index.ts
@@ -0,0 +1,5 @@
+export { Option, Some, None } from "./option";
+export { Result, Ok, Err } from "./result";
+export { AsyncResult } from "./async-result";
+export { attempt } from "./attempt";
+export { InvariantError } from "./errors";
