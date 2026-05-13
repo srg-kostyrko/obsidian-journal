f163bb6965b0bb4d4a0a26c34109bbe242276c03
Sergii Kostyrko <srg.kostyrko@gmail.com>
2026-05-13 22:16:07 +0300

    feat(host): wire createHostModule into main.ts and remove raw obsidian tokens

--- src/infrastructure/obsidian-tokens.ts
+++
@@ -1,7 +0,0 @@
-import { createToken } from "@/infrastructure/di";
-import type JournalPlugin from "@/main";
-
-import type { App } from "obsidian";
-
-export const PluginToken = createToken<JournalPlugin>("Plugin");
-export const ObsidianAppToken = createToken<App>("ObsidianApp");
--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,17 @@ import { Plugin } from "obsidian";
 import { CalendarModule } from "@/calendar";
 import { Container } from "@/infrastructure/di";
 import { FlowsModule } from "@/infrastructure/flows";
+import { createHostModule } from "@/infrastructure/host";
 import { LoggerModule } from "@/infrastructure/logger";
-import { ObsidianAppToken, PluginToken } from "@/infrastructure/obsidian-tokens";
 
 export default class JournalPlugin extends Plugin {
   #container?: Container;
 
   async onload(): Promise<void> {
     const container = new Container();
-    container.register(PluginToken).useValue(this);
-    container.register(ObsidianAppToken).useValue(this.app);
     container.addModule(LoggerModule);
     container.addModule(FlowsModule);
+    container.addModule(createHostModule(this));
     container.addModule(CalendarModule);
     await container.autoLoad();
     this.#container = container;
