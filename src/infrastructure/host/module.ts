c013119de451967d937ba822985369b5ffe42350
Sergii Kostyrko <srg.kostyrko@gmail.com>
2026-05-14 09:26:25 +0300

    feat(host): compose modals module into createHostModule

--- src/infrastructure/host/module.ts
+++ src/infrastructure/host/module.ts
@@ -4,10 +4,12 @@ import { NotesService } from "./internal/notes-service";
 import { PluginData } from "./internal/plugin-data";
 import { InternalObsidianAppToken, InternalPluginToken } from "./internal/tokens";
 import { WorkspaceService } from "./internal/workspace-service";
+import { createModalsModule } from "./modals/module";
 
 import type { Plugin } from "obsidian";
 
 export function createHostModule(plugin: Plugin): Module {
+  const modals = createModalsModule();
   return {
     register(c) {
       c.register(InternalPluginToken).useValue(plugin);
@@ -15,6 +17,7 @@ export function createHostModule(plugin: Plugin): Module {
       c.register(NotesService).useClass(NotesService).eager();
       c.register(WorkspaceService).useClass(WorkspaceService).eager();
       c.register(PluginData).useClass(PluginData);
+      modals.register(c);
     },
   };
 }
