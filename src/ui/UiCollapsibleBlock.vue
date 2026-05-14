bf826af281955faf3a3f630a867a7c18bd9cba72
Sergii Kostyrko <srg.kostyrko@gmail.com>
2026-05-14 18:51:39 +0300

    feat(ui): add UiCollapsibleBlock with v-model:expanded

---
+++ src/ui/UiCollapsibleBlock.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+import UiIcon from "./UiIcon.vue";
+
+const expanded = defineModel<boolean>("expanded");
+
+const icon = computed(() => (expanded.value ? "chevron-down" : "chevron-right"));
+
+function toggle() {
+  expanded.value = !expanded.value;
+}
+</script>
+
+<template>
+  <div class="collapsible-root" :data-open="expanded || null">
+    <div class="collapsible-trigger" @click="toggle">
+      <UiIcon :name="icon" />
+      <span class="collapsible-trigger-text">
+        <slot name="trigger" />
+      </span>
+      <span class="collapsible-trigger-controls" @click.stop>
+        <slot name="controls" />
+      </span>
+    </div>
+    <template v-if="expanded">
+      <slot />
+    </template>
+  </div>
+</template>
+
+<style scoped>
+.collapsible-root {
+  padding-bottom: var(--size-2-2);
+  margin-top: var(--size-2-2);
+  margin-bottom: var(--size-4-2);
+}
+.collapsible-root[data-open] {
+  border-bottom: 1px solid var(--color-accent);
+}
+.collapsible-trigger {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  border-top: 1px solid var(--color-accent);
+  border-bottom: 1px solid var(--color-accent);
+  gap: 4px;
+  padding-top: var(--size-2-2);
+  padding-bottom: var(--size-2-2);
+  min-height: 38px;
+}
+.collapsible-trigger-text {
+  flex-grow: 1;
+}
+</style>
