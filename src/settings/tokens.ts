3a653890c8db328535eb1c7c7704418050824fe9
Sergii Kostyrko <srg.kostyrko@gmail.com>
2026-05-14 12:16:32 +0300

    feat(settings): add DashboardBlockToken and SubpageToken

--- src/settings/tokens.ts
+++ src/settings/tokens.ts
@@ -1,7 +1,10 @@
 import { createMultiToken } from "@/infrastructure/di";
 
 import type { AnyCollectionDefinition, AnySliceDefinition, Migration } from "./schema";
+import type { DashboardBlock, AnySubpage } from "./ui/schema";
 
 export const SliceDefinitionToken = createMultiToken<AnySliceDefinition>("settings.slice");
 export const CollectionDefinitionToken = createMultiToken<AnyCollectionDefinition>("settings.collection");
 export const MigrationToken = createMultiToken<Migration>("settings.migration");
+export const DashboardBlockToken = createMultiToken<DashboardBlock>("settings.dashboardBlock");
+export const SubpageToken = createMultiToken<AnySubpage>("settings.subpage");
