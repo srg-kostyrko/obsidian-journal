9a97a0c010666944372e8d37e3f4019a6f899055
Sergii Kostyrko <srg.kostyrko@gmail.com>
2026-05-14 18:00:19 +0300

    feat(ui): add UiColorPicker primitive

---
+++ src/ui/UiColorPicker.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  disabled?: boolean;
+}>();
+
+const model = defineModel<string>();
+</script>
+
+<template>
+  <input v-model="model" type="color" :disabled="disabled" spellcheck="false" />
+</template>
