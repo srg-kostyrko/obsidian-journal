d4f8a29e2209651fb819e72b1c016603a7e075b3
Sergii Kostyrko <srg.kostyrko@gmail.com>
2026-05-14 18:16:57 +0300

    feat(ui): add UiIconedRow primitive

---
+++ src/ui/UiIconedRow.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import UiIcon from "./UiIcon.vue";
+
+defineProps<{ icon: string }>();
+</script>
+
+<template>
+  <div class="iconed-row">
+    <UiIcon :name="icon" />
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.iconed-row {
+  display: flex;
+  align-items: center;
+  gap: var(--size-2-2);
+}
+</style>
