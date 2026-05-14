f1938937eb85e8e3a278a9ebc4a3d6dc466a525e
Sergii Kostyrko <srg.kostyrko@gmail.com>
2026-05-14 08:55:22 +0300

    feat(host): add modal definition types

---
+++ src/infrastructure/host/modals/types.ts
@@ -0,0 +1,21 @@
+import type { Component } from "vue";
+
+export interface ModalDefinitionInput<TProps, _TResult> {
+  component: Component;
+  title: (props: TProps) => string;
+  width?: number | ((props: TProps) => number);
+  cssClass?: string | readonly string[];
+}
+
+export interface ModalDefinition<TProps, TResult> {
+  readonly component: Component;
+  readonly title: (props: TProps) => string;
+  readonly width: ((props: TProps) => number) | undefined;
+  readonly cssClass: readonly string[];
+  readonly __result: (witness: never) => TResult;
+}
+
+export interface ModalApi<TResult> {
+  submit: (value: TResult) => void;
+  cancel: () => void;
+}
