463c1d3f3d141768e104b20a5d08c30d210c83a7
Sergii Kostyrko <srg.kostyrko@gmail.com>
2026-05-14 18:08:41 +0300

    feat(ui): add UiIconButton primitive

---
+++ src/ui/UiIconButton.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import UiButton from "./UiButton.vue";
+import UiIcon from "./UiIcon.vue";
+
+defineProps<{
+  icon: string;
+  tooltip?: string;
+}>();
+</script>
+
+<template>
+  <UiButton :tooltip="tooltip" flat class="icon-button">
+    <UiIcon :name="icon" />
+  </UiButton>
+</template>
+
+<style scoped>
+.icon-button {
+  padding: var(--size-4-1) var(--size-4-2);
+}
+</style>
