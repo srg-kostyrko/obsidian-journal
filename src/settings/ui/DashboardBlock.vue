386cd8b98dc671b89f0e69bb06be382b825facdb
Sergii Kostyrko <srg.kostyrko@gmail.com>
2026-05-14 12:41:50 +0300

    feat(settings): add DashboardBlock wrapper component

---
+++ src/settings/ui/DashboardBlock.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { Component } from "vue";
+
+defineProps<{ component: Component }>();
+</script>
+
+<template>
+  <section class="journal-settings-block">
+    <component :is="component" />
+  </section>
+</template>
