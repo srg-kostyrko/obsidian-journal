2f1bc2cdd971bd003dc56386b814379c6003530a
Sergii Kostyrko <srg.kostyrko@gmail.com>
2026-05-14 17:48:59 +0300

    feat(ui): add UiNumberInput primitive

---
+++ src/ui/UiNumberInput.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+  placeholder?: string;
+  disabled?: boolean;
+  min?: number;
+  max?: number;
+  narrow?: boolean;
+}>();
+
+const model = defineModel<number>();
+</script>
+
+<template>
+  <input
+    v-model="model"
+    type="number"
+    :class="{ 'narrow-input': narrow }"
+    :placeholder="placeholder"
+    :disabled="disabled"
+    :min="min"
+    :max="max"
+    spellcheck="false"
+  />
+</template>
+
+<style scoped>
+.narrow-input {
+  width: 60px;
+}
+</style>
