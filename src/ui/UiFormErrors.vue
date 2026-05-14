b40cfc3996a8b3d87e69d2c73b8fc0a099b5455f
Sergii Kostyrko <srg.kostyrko@gmail.com>
2026-05-14 18:14:51 +0300

    feat(ui): add UiFormErrors primitive

---
+++ src/ui/UiFormErrors.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  errors?: string[];
+}>();
+</script>
+
+<template>
+  <ul v-if="errors && errors.length > 0" class="journal-errors">
+    <li v-for="error in errors" :key="error">{{ error }}</li>
+  </ul>
+</template>
+
+<style scoped>
+.journal-errors {
+  color: var(--text-error);
+}
+</style>
