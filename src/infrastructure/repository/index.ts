6228eebdef0b6c63f0a5bfe1731b5b34ec1c8bb9
Sergii Kostyrko <srg.kostyrko@gmail.com>
2026-05-23 11:30:42 +0300

    feat(repository): add BaseRepository with id-key safe update

--- src/infrastructure/repository/index.ts
+++ src/infrastructure/repository/index.ts
@@ -1,2 +1,3 @@
+export { BaseRepository } from "./base-repository";
 export { RepositoryQuery } from "./repository-query";
 export type { RepositoryContract, RepositoryEvents, RepositoryQueryContract } from "./types";
