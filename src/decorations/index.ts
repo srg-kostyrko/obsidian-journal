6ebd37e4dcb9231f53177b0ebd529e727a26d120
Sergii Kostyrko <srg.kostyrko@gmail.com>
2026-05-27 09:55:47 +0300

    feat(decorations): export colorToString from barrel

--- src/decorations/index.ts
+++ src/decorations/index.ts
@@ -30,6 +30,7 @@ export { DecorationEngine, periodMatchesWrite, type DecorationBinding } from "./
 export { decorationsModule } from "./module";
 export { CellDecorationMapKey, type CellStyleRef } from "./ui/cell-decoration-map-key";
 export { default as CellDecoration } from "./ui/CellDecoration.vue";
+export { colorToString } from "./ui/color";
 export { default as DecorationPreview } from "./ui/DecorationPreview.vue";
 export { useCellDecorations } from "./use-cell-decorations";
 export {
