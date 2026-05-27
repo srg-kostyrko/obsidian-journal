02cd26f69507b5242c8abbe1544b725a9e17f7a2
Sergii Kostyrko <srg.kostyrko@gmail.com>
2026-05-27 10:25:56 +0300

    feat(code-blocks): register navigation block in module

--- src/code-blocks/module.ts
+++ src/code-blocks/module.ts
@@ -2,9 +2,11 @@ import type { Module } from "@/infrastructure/di";
 import { CodeBlockDefinitionToken } from "@/infrastructure/host";
 
 import { homeCodeBlock } from "./home/home-block";
+import { navigationCodeBlock } from "./nav/nav-block";
 
 export const codeBlocksModule: Module = {
   register(c) {
     c.register(CodeBlockDefinitionToken).useValue(homeCodeBlock);
+    c.register(CodeBlockDefinitionToken).useValue(navigationCodeBlock);
   },
 };
