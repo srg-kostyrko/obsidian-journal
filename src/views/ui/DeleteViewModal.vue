785181be116a1a7c58ef4b63c3922546b40d156f
Sergii Kostyrko <srg.kostyrko@gmail.com>
2026-05-28 13:51:01 +0300

    feat(views): DeleteViewModal

--- src/views/ui/DeleteViewModal.vue
+++ src/views/ui/DeleteViewModal.vue
@@ -1,1 +1,23 @@
-<template><div /></template>
+<script setup lang="ts">
+import { m } from "@/i18n";
+import { useModal } from "@/infrastructure/host/modals";
+import UiButton from "@/ui/UiButton.vue";
+import UiSettingRow from "@/ui/UiSettingRow.vue";
+
+defineProps<{ viewName: string }>();
+
+const api = useModal();
+</script>
+
+<template>
+  <div>
+    <UiSettingRow>
+      <template #description>{{ m.view_delete_modal_description() }}</template>
+    </UiSettingRow>
+
+    <UiSettingRow controls-only>
+      <UiButton @click="api.cancel()">{{ m.common_action_cancel() }}</UiButton>
+      <UiButton cta warning @click="api.submit()">{{ m.common_action_delete() }}</UiButton>
+    </UiSettingRow>
+  </div>
+</template>
