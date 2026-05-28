4ce6325443d93f02aa65897526ef39c8e5d92bed
Sergii Kostyrko <srg.kostyrko@gmail.com>
2026-05-28 11:22:55 +0300

    feat(views): tokens + events shape

---
+++ src/views/tokens.ts
@@ -0,0 +1,13 @@
+import { createMultiToken, createToken } from "@/infrastructure/di";
+import type { RepositoryEvents } from "@/infrastructure/repository";
+
+import type { View, ViewId } from "./config";
+import type { ViewBlockDefinition } from "./define-view-block";
+import type { Emitter } from "nanoevents";
+
+// BaseRepository emits `created(id)`, `updated(id, changes)`, `deleted(id)` for us;
+// we do not add custom events at foundation-time.
+export type ViewsEvents = RepositoryEvents<ViewId, View>;
+
+export const ViewBlockDefinitionToken = createMultiToken<ViewBlockDefinition>("views.block");
+export const ViewsEventsToken = createToken<Emitter<ViewsEvents>>("views.events");
