37a590a80c310320dceb78840a77af434e7c1635
Sergii Kostyrko <srg.kostyrko@gmail.com>
2026-06-02 09:32:54 +0300

    test(migrations): end-to-end legacy migration chain

---
+++ src/settings/legacy/index.ts
@@ -0,0 +1,9 @@
+import type { Migration } from "@/settings";
+
+import { v1ToV2Migration } from "./v1-to-v2";
+import { v2ToV3Migration } from "./v2-to-v3";
+import { v3ToV4Migration } from "./v3-to-v4";
+
+export { pendingNoteMigrationSlice } from "./pending-note-migration";
+
+export const legacyMigrations: readonly Migration[] = [v1ToV2Migration, v2ToV3Migration, v3ToV4Migration];
