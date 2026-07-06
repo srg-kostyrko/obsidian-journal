259ecceb3915e9ee97822ef638a98721a57d2adc
Sergii Kostyrko <srg.kostyrko@gmail.com>
2026-07-06 20:46:31 +0300

    fix(views): route toolbar button tooltip through UiButton prop
    Passing both :aria-label and :title fell through to the <button>,
    producing an Obsidian tooltip and a native browser tooltip at once.
    Use UiButton's tooltip prop like every other consumer.

--- src/views/toolbar-items/button/ui/ButtonItem.vue
+++ src/views/toolbar-items/button/ui/ButtonItem.vue
@@ -123,7 +123,7 @@ function onClick(event: MouseEvent): void {
 </script>
 
 <template>
-  <UiButton flat :aria-label="tooltip" :title="tooltip" @click="onClick" @auxclick.middle.prevent="onClick">
+  <UiButton flat :tooltip="tooltip" @click="onClick" @auxclick.middle.prevent="onClick">
     <UiIcon v-if="icon" :name="icon" />
     <span v-if="label">{{ label }}</span>
     <span v-else-if="!icon">{{ tooltip }}</span>
